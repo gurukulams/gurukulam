054b5609845c48b3a1a8f963b0ec48754c1afed2
Sathish Kumar Thiyagarajan <sathishkumar.thiyagarajan@gmail.com>
2020-05-17 00:14:07 +0800

    fa added

--- src/js/home.js
+++ src/js/home.js
@@ -1,42 +1,125 @@
 import { html } from "lit-html";
 
 export let home = html`
-	<form class="form-signin">
-		<img
-			class="mb-4"
-			src="/docs/4.5/assets/brand/bootstrap-solid.svg"
-			alt=""
-			width="72"
-			height="72"
-		/>
-		<h1 class="h3 mb-3 font-weight-normal">Please sign in</h1>
-		<label for="inputEmail" class="sr-only">Email address</label>
-		<input
-			type="email"
-			id="inputEmail"
-			class="form-control"
-			placeholder="Email address"
-			required
-			autofocus
-		/>
-		<label for="inputPassword" class="sr-only">Password</label>
-		<input
-			type="password"
-			id="inputPassword"
-			class="form-control"
-			placeholder="Password"
-			required
-		/>
-		<div class="checkbox mb-3">
-			<label> <input type="checkbox" value="remember-me" /> Remember me </label>
-		</div>
-		<a
-			href="#"
-			onclick="onNavigate('/about'); return false;"
-			class="btn btn-lg btn-primary btn-block"
-		>
-			Sign in
+	<nav class="navbar navbar-expand-sm bg-dark navbar-dark">
+		<!-- Brand/logo -->
+		<a class="navbar-brand" href="#">
+			<img
+				src="https://techatpark.com/images/icons/logo.svg"
+				width="30"
+				height="30"
+				class="d-inline-block align-top"
+				alt=""
+			/>
+			GURUKULAM
 		</a>
-		<p class="mt-5 mb-3 text-muted">&copy; 2017-2020</p>
-	</form>
+	</nav>
+
+	<section class="side-bar-warp">
+		<div class="sidebar-menu small-side-bar flowHide">
+			<nav class="">
+				<ul class="navbar-nav">
+					<li class="nav-item">
+						<a class="nav-link" href="#">
+							<span class="sidebar-icon"><i class="fas fa-columns"></i></span>
+							<span
+								class="fadeInRight animated nav-link-name name-hide tax-show"
+								>Dashboard</span
+							>
+						</a>
+					</li>
+					<li class="nav-item">
+						<a class="nav-link" href="#">
+							<span class="sidebar-icon"
+								><i class="far fa-question-circle"></i
+							></span>
+							<span
+								class="fadeInRight animated nav-link-name name-hide tax-show"
+								>Creat a Quest</span
+							>
+						</a>
+					</li>
+					<li class="nav-item">
+						<a class="nav-link" href="#">
+							<span class="sidebar-icon"
+								><img src="images/manage-quests.png" alt=""
+							/></span>
+							<span
+								class="fadeInRight animated nav-link-name name-hide tax-show"
+								>Manage Quests</span
+							>
+						</a>
+					</li>
+					<li class="nav-item">
+						<a class="nav-link" href="#">
+							<span class="sidebar-icon"
+								><img src="images/leaderboards.png" alt=""
+							/></span>
+							<span
+								class="fadeInRight animated nav-link-name name-hide tax-show"
+								>Leaderboards</span
+							>
+						</a>
+					</li>
+					<li class="nav-item">
+						<a class="nav-link" href="#">
+							<span class="sidebar-icon"
+								><img src="images/one-on-one.png" alt=""
+							/></span>
+							<span
+								class="fadeInRight animated nav-link-name name-hide tax-show"
+								>One On Ones</span
+							>
+						</a>
+					</li>
+					<li class="nav-item">
+						<a class="nav-link" href="#">
+							<span class="sidebar-icon"
+								><img src="images/give-kudos.png" alt=""
+							/></span>
+							<span
+								class="fadeInRight animated nav-link-name name-hide tax-show"
+								>Give Kudos</span
+							>
+						</a>
+					</li>
+
+					<li class="nav-item">
+						<a class="nav-link" href="#">
+							<span class="sidebar-icon"
+								><img src="images/settings.png" alt=""
+							/></span>
+							<span
+								class="fadeInRight animated nav-link-name name-hide tax-show"
+								>Nation Settings</span
+							>
+						</a>
+					</li>
+					<li class="nav-item">
+						<a class="nav-link" href="#">
+							<span class="sidebar-icon"
+								><img src="images/feedback.png" alt=""
+							/></span>
+							<span
+								class="fadeInRight animated nav-link-name name-hide tax-show"
+								>Help/Feedback</span
+							>
+						</a>
+					</li>
+
+					<li class="nav-item">
+						<a class="nav-link" href="#">
+							<span class="sidebar-icon"
+								><img src="images/log-out.png" alt=""
+							/></span>
+							<span
+								class="fadeInRight animated nav-link-name name-hide tax-show"
+								>Log Out</span
+							>
+						</a>
+					</li>
+				</ul>
+			</nav>
+		</div>
+	</section>
 `;
