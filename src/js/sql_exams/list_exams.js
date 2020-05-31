79fb0bf2e9179c1e012ecfd020e96a3e8c26ed38
Sathish Kumar Thiyagarajan <sathishkumar.thiyagarajan@gmail.com>
2020-05-31 14:51:27 +0800

    changes cotents

--- src/js/sql_exams/list_exams.js
+++ src/js/sql_exams/list_exams.js
@@ -5,21 +5,26 @@ export let list = html`
 		<thead>
 			<tr>
 				<th scope="col">#</th>
-				<th scope="col">First</th>
-				<th scope="col">Last</th>
+				<th scope="col">Exam Name</th>
+				<th scope="col">Exam Type</th>
 			</tr>
 		</thead>
 		<tbody>
 			<tr>
 				<th scope="row">1</th>
-				<td>Mark</td>
-				<td>Otto</td>
+				<td>Product DB</td>
+				<td>Potgres</td>
+			</tr>
+			<tr>
+				<th scope="row">2</th>
+				<td>HR DB</td>
+				<td>MySQL</td>
 			</tr>
 		</tbody>
 	</table>
 	<ul class="nav justify-content-end">
 		<li class="nav-item">
-			<a class="nav-link active" href="#" onclick="onNavigate('/sql_exams/exam')">Create</a>
+			<a class="nav-link active" href="javascript://" onclick="onNavigate('/sql_exams/exam')">Create</a>
 		</li>
 	</ul>
 `;
