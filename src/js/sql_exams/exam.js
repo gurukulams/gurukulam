0b0387d28934975b7eda23a50b77f331cf906762
rm.hari <rm.haripriya@gmail.com>
2020-06-01 21:51:08 +0530

    exam page updated

--- src/js/sql_exams/exam.js
+++ src/js/sql_exams/exam.js
@@ -6,27 +6,37 @@ export let exam = html`
 			<main role="main" class="col-md-12 ml-sm-auto col-lg-12 px-md-4">
 				<div
 					class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom"
-				>
-					<h1 class="h2">Exam</h1>					
-				</div>
+			  	>
+					<h6 class="h2">Create a new exam</h6>					
+        </div>
+        
 				<form class="text-left">
-  <div class="form-group">
-    <label for="exampleInputEmail1">Email address</label>
-    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
-    <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
-  </div>
-  <div class="form-group">
-    <label for="exampleInputPassword1">Password</label>
-    <input type="password" class="form-control" id="exampleInputPassword1">
-  </div>
-  <div class="form-group form-check">
-    <input type="checkbox" class="form-check-input" id="exampleCheck1">
-    <label class="form-check-label" for="exampleCheck1">Check me out</label>
-  </div>
-  <button class="btn btn-primary" onclick="onNavigate('')">Save</button>
-  <button class="btn btn-secondary">Cancel</button>
-</form>
-			</main>
-		</div>
-	</div>
+
+              <div class="form-group">
+                 <label for="examName">Name of Exam</label>
+                 <input type="email" class="form-control" id="examName">
+              </div>
+
+
+              <div class="form-group">
+                  <label for="database">Database</label>
+                  <select class="form-control" id="database">
+                          <option>POSTGRES</option>
+                          <option>MYSQL</option>
+      
+                  </select>
+              </div>
+
+              <div class="form-group">
+                  <label for="scriptUpload">Upload Script</label>
+                  <input type="file" class="form-control-file" id="scriptUpload">
+              </div>
+
+             <button class="btn btn-primary" onclick="onNavigate('')">Save</button>
+             <button class="btn btn-secondary">Cancel</button>
+
+        </form>
+
+      </main>
+
 `;
