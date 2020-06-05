1ba544cd2b5ff06acc209aa81638b604f2ca3fab
rm.hari <rm.haripriya@gmail.com>
2020-06-05 11:00:11 +0530

    changes in question page

--- src/js/sql_exams/question.js
+++ src/js/sql_exams/question.js
@@ -3,30 +3,18 @@ import { html } from 'lit-html';
 export let question = html`
 	<body class="text-center">
 		<nav class="navbar navbar-expand navbar-dark bg-dark">
-			<div class="col-11 col-md-11 col-lg-11 text-left">
-				<a class="navbar-brand" href="#">
-					<img
-						src="https://techatpark.com/images/icons/logo_full.svg"
-						width="30"
-						height="30"
-						class="d-inline-block align-top"
-						alt=""
-						loading="lazy"
-					/>
-					Product DB - QNumber - Indicaorr - CLue - View - KIDS Mode
-				</a>
+		<div class="col-8 col-md-8 col-lg-8 text-left">
+				Database TYpe
 			</div>
-			<div class="col-3 col-md-3 col-lg-3">
-				<button type="button" class="btn btn-success">Previous</button>
+			<div class="col-4 col-md-4 col-lg-4">
+			<button type="button" class="btn btn-info ">Try Our KIDS mode!!!..</button>
+				<button type="button" class="btn btn-success float-right">Run</button>
 
-				<button type="button" class="btn btn-success">Run</button>
-
-				<button type="button" class="btn btn-success">Next</button>
 			</div>
 		</nav>
 
 		<div class="container vh-100">
-			<div class="row h-100 form-group">
+			<div class="row h-75 form-group">
 				<div class="col-6 col-md-6 col-lg-6 mt-4 border-right">
 					<label for="question">Question:</label>
 					<div id="rich-text-editor"></div>
@@ -35,7 +23,20 @@ export let question = html`
 					<label for="answer">Answer:</label>
 					<div id="editor"></div>
 				</div>
+				
+
 			</div>
 		</div>
+		<nav class="navbar navbar-expand bg-secondary text-dark">
+			<div class="col-4 col-md-4 col-lg-4 ">
+			<button type="button" class="btn btn-primary">Previous</button>
+			</div>
+			<div class="col-4 col-md-4 col-lg-4 text-center">
+			<button type="button" class="btn btn-warning">Clue</button>
+			</div>
+			<div class="col-4 col-md-4 col-lg-4 text-center float-right">
+			<button type="button" class="btn btn-primary">Next</button>
+			</div>
+		
 	</body>
 `;
