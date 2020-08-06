606710095c331f5874899f85dca1aa232703e138
apt-get2Update <thirumurugan3131@gmail.com>
2020-08-06 20:18:14 +0530

    examination list added

--- src/js/sql_exams/exam.js
+++ src/js/sql_exams/exam.js
@@ -1,5 +1,45 @@
 import { html } from 'lit-html';
+// console.log(fetch)
 
+let data = {
+	name:"",
+	type:""
+};
+const clickHandler = {
+	// handleEvent method is required.
+	handleEvent(e) { 
+	  console.log('clicked!',e.target.value);
+	  console.log(data);
+	},
+
+	// fetch("","POST",)
+	// event listener objects can also define zero or more of the event 
+	// listener options: capture, passive, and once.
+	capture: true,
+  };
+
+  const onChangeType= {
+	handleEvent(e) { 
+		console.log('clicked!',e.target.value);
+		data.type= e.target.value;
+	},
+  
+	  // fetch("","POST",)
+	  // event listener objects can also define zero or more of the event 
+	  // listener options: capture, passive, and once.
+	  capture: true,
+  };
+  const onChangeName= {
+	handleEvent(e) { 
+		console.log('clicked!',e.target.value);
+	  },
+  
+	  // fetch("","POST",)
+	  // event listener objects can also define zero or more of the event 
+	  // listener options: capture, passive, and once.
+	  capture: true,
+  };
+  
 export let exam = html`
 	<main role="main" class="col-md-12 ml-sm-auto col-lg-12 px-md-4">
 		<div
@@ -8,15 +48,14 @@ export let exam = html`
 			<h6 class="h2">Create a new exam</h6>
 		</div>
 
-		<form class="text-left">
 			<div class="form-group">
 				<label for="examName">Name of Exam</label>
-				<input class="form-control" />
+				<input class="form-control" @change=${onChangeName} .value={data.name} />
 			</div>
 
 			<div class="form-group">
 				<label for="database">Database</label>
-				<select class="form-control">
+				<select class="form-control" @change=${onChangeType} .value=${data.type}>
 					<option>POSTGRES</option>
 					<option>MYSQL</option>
 				</select>
@@ -28,7 +67,6 @@ export let exam = html`
 			</div>
 
 			<button class="btn btn-secondary">Cancel</button>
-			<button class="btn btn-primary" onclick="onNavigate('#/sql_exams')">Save</button>
-		</form>
+			<button class="btn btn-primary" @click=${clickHandler}>dai</button>
 	</main>
 `;
--- src/js/sql_exams/list_exams.js
+++ src/js/sql_exams/list_exams.js
@@ -1,5 +1,39 @@
 import { html } from 'lit-html';
 
+import {until} from 'lit-html/directives/until.js';
+
+
+const fetchExamas = async () => {
+	let myHeaders = new Headers();
+	myHeaders.append('Content-Type', 'application/json');
+	myHeaders.append(
+		'authorization',
+		'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MSwiaWF0IjoxNTk2NDcyODM3LCJleHAiOjE1OTkwNjQ4Mzd9.5C2r_taYylA1DNlaHPNgDSeA9zid51nCI6SLZo-boBg',
+	);
+	const format = {
+		method: 'GET',
+		headers: myHeaders,
+	};
+	let myRequest = new Request('http://ec2-13-126-173-216.ap-south-1.compute.amazonaws.com/exams');
+	const data = await fetch(myRequest, format).then((r) => r.json());
+	return renderExam(data);
+};
+const renderExam = (db) => {
+	return db.map(
+		(i) => html`
+			<tr>
+				<td>${i.name}</td>
+				<td>${i.type}</td>
+				<td>
+					<a class="nav-link active" href="javascript://" onclick="onNavigate('#/sql_exams/exam/question')"
+						>Add Question</a
+					>
+				</td>
+			</tr>
+		`,
+	);
+};
+
 export let list = html`
 	<table class="table">
 		<thead>
@@ -10,24 +44,7 @@ export let list = html`
 			</tr>
 		</thead>
 		<tbody>
-			<tr>
-				<td>Product DB</td>
-				<td>Potgres</td>
-				<td>
-					<a class="nav-link active" href="javascript://" onclick="onNavigate('#/sql_exams/exam/question')"
-						>Add Question</a
-					>
-				</td>
-			</tr>
-			<tr>
-				<td>HR DB</td>
-				<td>MySQL</td>
-				<td>
-					<a class="nav-link active" href="javascript://" onclick="onNavigate('#/sql_exams/exam/question')"
-						>Take Exam</a
-					>
-				</td>
-			</tr>
+			data:${until(fetchExamas(), html`<span>Loading...</span>`)}
 		</tbody>
 	</table>
 	<ul class="nav justify-content-end">
