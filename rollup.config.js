fed344e64ef7702a839a49d4fd02e90a957e2dba
apt-get2Update <thirumurugan3131@gmail.com>
2021-03-06 15:22:48 +0530

    thiru | practice

--- rollup.config.js
+++ rollup.config.js
@@ -10,4 +10,11 @@ export default [{
     file: 'dist/js/bundle-sql.js',
     format: 'cjs'
   }
-}];
+},
+{
+  input: 'src/js/practice/index.js',
+  output: {
+    file: 'dist/js/practice-bundle.js',
+    format: 'cjs'
+  }}];
+
