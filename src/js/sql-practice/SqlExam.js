d300ccb9dddea7107be7aa67c069f6de6a44d8de
sathishk <sathishkumar.thiyagarajan@gmail.com>
2021-03-08 13:53:27 +0530

    ssss

--- src/js/sql-practice/SqlExam.js
+++ src/js/sql-practice/SqlExam.js
@@ -2,24 +2,25 @@ class SqlExam {
 
   constructor(_parent) {
     this.parent = _parent;
+    
+    
+  }
+
+  render() {
+
     this.oldChildNodes = [];
 
     while (this.parent.firstChild) {
       this.oldChildNodes.push(this.parent.removeChild(this.parent.firstChild));
     }
-
+    
     let formEl = document.createElement('form');
     formEl.classList.add("row");
     formEl.classList.add("g-3");
     formEl.innerHTML = '<div class="col-md-6"><label for="inputEmail4" class="form-label">Name</label><input type="email" class="form-control" id="inputEmail4"></div><div class="col-md-6"><label for="inputState" class="form-label">Type</label><select id="inputState" class="form-select"><option selected>Choose...</option><option>Postgres</option></select></div><div class="col-12"><label for="inputAddress" class="form-label">Description</label><textarea class="form-control" id="inputAddress"></textarea></div> <div class="col-md-6"><label for="formFile" class="form-label">Scripts</label><input class="form-control" type="file" id="formFile"></div><div class="col-12"><button type="submit" class="btn btn-primary">Create</button> <button class="btn btn-secondary">Cancel</button></div>';
     this.parent.appendChild(formEl);
 
-    this.render();
     this.registerEvents();
-  }
-
-  render() {
-
   }
 
   registerEvents() {
--- src/js/sql-practice/SqlExams.js
+++ src/js/sql-practice/SqlExams.js
@@ -4,15 +4,31 @@ class SqlExams {
 
   constructor(_parent) {
     this.parent = _parent;
+    this.sqlExam = new SqlExam(_parent);
     this.render();
   }
 
   render() {
-    this.parent.innerHTML = '<nav class="navbar navbar-expand-lg navbar-light bg-light rounded" aria-label="Eleventh navbar example"> <div class="container-fluid"> <div class="collapse navbar-collapse" id="navbarsExample09"> <ul class="navbar-nav me-auto mb-2 mb-lg-0 pagination"> <li class="page-item"><a class="page-link" href="#">Previous</a></li> <li class="page-item"><a class="page-link" href="#">1</a></li> <li class="page-item"><a class="page-link" href="#">2</a></li> <li class="page-item"><a class="page-link" href="#">3</a></li> <li class="page-item"><a class="page-link" href="#">Next</a></li> </ul> <form> <button type="button" class="btn btn-primary">Add</button> </form> </div> </div> </nav> <div class="container"> <div class="items"> <ul class="list-group"> <li class="list-group-item"> <div class="d-flex w-100 justify-content-between"> <h5 class="mb-1">List group item heading</h5> <small>3 days ago</small> </div> <p class="mb-1">Some placeholder content in a paragraph.</p> <small>And some small print.</small> </li> <li class="list-group-item"> <div class="d-flex w-100 justify-content-between"> <h5 class="mb-1">List group item heading</h5> <small>3 days ago</small> </div> <p class="mb-1">Some placeholder content in a paragraph.</p> <small>And some small print.</small> </li> </ul> </div> </div>';
 
     fetch('/api/exams/sql')
-      .then(response => response.json())
+      .then(response => {
+        if (response.status == 200) {
+          return response;
+        }
+        else if (response.status == 204) {
+          let e = new Error(response.statusText);
+          e.name = "NoContent";
+          e.root = this;
+          throw e;
+        }
+        throw Error(response.statusText);
+      })
+      .then(function (response) {
+        return response.json();
+      })
       .then(page => {
+        this.parent.innerHTML = '<nav class="navbar navbar-expand-lg navbar-light bg-light rounded" aria-label="Eleventh navbar example"> <div class="container-fluid"> <div class="collapse navbar-collapse" id="navbarsExample09"> <ul class="navbar-nav me-auto mb-2 mb-lg-0 pagination"> <li class="page-item"><a class="page-link" href="#">Previous</a></li> <li class="page-item"><a class="page-link" href="#">1</a></li> <li class="page-item"><a class="page-link" href="#">2</a></li> <li class="page-item"><a class="page-link" href="#">3</a></li> <li class="page-item"><a class="page-link" href="#">Next</a></li> </ul> <form> <button type="button" class="btn btn-primary">Add</button> </form> </div> </div> </nav> <div class="container"> <div class="items"> <ul class="list-group"> <li class="list-group-item"> <div class="d-flex w-100 justify-content-between"> <h5 class="mb-1">List group item heading</h5> <small>3 days ago</small> </div> <p class="mb-1">Some placeholder content in a paragraph.</p> <small>And some small print.</small> </li> <li class="list-group-item"> <div class="d-flex w-100 justify-content-between"> <h5 class="mb-1">List group item heading</h5> <small>3 days ago</small> </div> <p class="mb-1">Some placeholder content in a paragraph.</p> <small>And some small print.</small> </li> </ul> </div> </div>';
+
         let ulEl = this.parent.querySelector('.list-group');
         ulEl.innerHTML = '';
         const sqlExams = page.content;
@@ -20,7 +36,18 @@ class SqlExams {
           ulEl.appendChild(this.createListElement(item));
         });
         this.registerEvents();
-      });
+      }).catch(function (error) {
+        if (error.name == "NoContent") {
+          error.root.parent.innerHTML = '<p class="lead">There are no exams. But you can create one <a href="javascript://">here</a></p>';
+          error.root.parent.querySelector("p > a").addEventListener('click', event => {
+            error.root.sqlExam.render();
+          });
+        }
+        else {
+          console.log('Request failed:', error);
+        }
+
+      });;
   }
 
   registerEvents() {
@@ -38,7 +65,7 @@ class SqlExams {
               }
             })
               .then(response => {
-                if(response.status == 200) {
+                if (response.status == 200) {
                   this.render();
                 }
               })
@@ -50,7 +77,7 @@ class SqlExams {
     this.parent
       .querySelector('#navbarsExample09 > form > button.btn-primary')
       .addEventListener('click', event => {
-        new SqlExam(this.parent);
+        this.sqlExam.render();
       });
   }
 
