a8f6f9edaa44bc3fbab7928187c1b4242d489815
sathishk <sathishkumar.thiyagarajan@gmail.com>
2021-06-05 07:20:21 +0530

    ssssssdsdsd

--- src/js/books/Chapter.js
+++ src/js/books/Chapter.js
@@ -2,36 +2,79 @@ class Chapter {
   constructor(_parent) {
     this.parent = _parent;
 
-    this.strikes = [
-      { note: "DDDDDD", text: "as plain text.", prevWord: "executed" },
-    ];
-
-    this.strikes.forEach((strike) => this.highlightNotes(strike));
-
     // eslint-disable-next-line no-undef
     const myModal = new bootstrap.Modal(
       document.getElementById("noteModel"),
       {}
     );
 
+    const editNote = (event) => {
+      myModal.selectedMark = event.currentTarget;
+      myModal.show();
+    };
+
+    this.strikes = [];
+
+    const highlightNotes = (strike) => {
+      if (strike.text !== "") {
+        let text = this.parent.innerHTML;
+        //let re = new RegExp(strike.prevWord + " " + strike.text, "g"); // search for all instances
+
+        let marked = strike.note
+          ? `<mark data-bs-toggle="tooltip" data-bs-placement="top" title="${strike.note}">
+        ${strike.text}
+        </mark>`
+          : `<mark>
+        ${strike.text}
+        </mark>`;
+        let newText = text.replace(
+          strike.prevWord + " " + strike.text,
+          strike.prevWord + " " + marked
+        );
+        this.parent.innerHTML = newText;
+        _parent.querySelectorAll("mark").forEach((markEl) => {
+          markEl.addEventListener("dblclick", (event) => {
+            editNote(event);
+          });
+        });
+      }
+    };
+
+    this.strikes.forEach((strike) => highlightNotes(strike));
+
+    _parent.querySelectorAll("mark").forEach((markEl) => {
+      markEl.addEventListener("dblclick", (event) => {
+        editNote(event);
+      });
+    });
+
     document
       .getElementById("noteModel")
       .querySelector(".btn-primary")
       .addEventListener("click", () => {
-        console.log(myModal.selection);
+        const note = document.getElementById("noteText").value.trim();
+        if (note === "") {
+          myModal.selectedMark.removeAttribute("title");
+          myModal.selectedMark.removeAttribute("data-bs-toggle");
+          myModal.selectedMark.removeAttribute("top");
+        } else {
+          myModal.selectedMark.setAttribute("title", note);
+        }
         myModal.hide();
       });
 
     document
       .getElementById("noteModel")
       .addEventListener("shown.bs.modal", function () {
+        document.getElementById("noteText").value =
+          myModal.selectedMark.getAttribute("title");
         document.getElementById("noteText").focus();
       });
 
     document
       .getElementById("noteModel")
       .addEventListener("hide.bs.modal", function () {
-        myModal.selection = null;
+        myModal.selectedMark = null;
       });
 
     _parent.addEventListener("mousedown", () => {
@@ -54,29 +97,10 @@ class Chapter {
           strike.prevWord = prevWord;
           // strike.note = "DDDDQWQWQ";
           this.strikes.push(strike);
-          this.highlightNotes(strike);
+          highlightNotes(strike);
         }
       }
     });
-
-    _parent.addEventListener("dblclick", () => {});
-  }
-
-  highlightNotes(strike) {
-    if (strike.text !== "") {
-      let text = this.parent.innerHTML;
-      let re = new RegExp(strike.prevWord + " " + strike.text, "g"); // search for all instances
-
-      let marked = strike.note
-        ? `<mark data-bs-toggle="tooltip" data-bs-placement="top" title="${strike.note}">
-      ${strike.text}
-      </mark>`
-        : `<mark>
-      ${strike.text}
-      </mark>`;
-      let newText = text.replace(re, strike.prevWord + " " + marked);
-      this.parent.innerHTML = newText;
-    }
   }
 }
 
