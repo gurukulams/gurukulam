ce4b0e7f9ca7ed16d33f8727b850b1978e5ba07c
haripriya <rm.haripriya@gmail.com>
2022-04-24 20:38:10 +0530

    Cleanup Lint Code

--- rollup.config.js
+++ rollup.config.js
@@ -1,11 +1,23 @@
+import eslint from '@rollup/plugin-eslint';
+
 export default [{
   input: 'src/js/core/index.js',
+  plugins: [
+    eslint({
+      /* your options */
+    })
+  ],
   output: {
     file: 'dist/js/core.js',
     format: 'cjs'
   }
 }, {
   input: 'src/js/oauth/index.js',
+  plugins: [
+    eslint({
+      /* your options */
+    })
+  ],
   output: {
     file: 'dist/js/oauth.js',
     format: 'cjs'
@@ -13,6 +25,11 @@ export default [{
 },
 {
   input: 'src/js/main/index.js',
+  plugins: [
+    eslint({
+      /* your options */
+    })
+  ],
   output: {
     file: 'dist/js/index.js',
     format: 'cjs'
@@ -20,6 +37,11 @@ export default [{
 },
 {
   input: 'src/js/practice/index.js',
+  plugins: [
+    eslint({
+      /* your options */
+    })
+  ],
   output: {
     file: 'dist/js/practices.js',
     format: 'cjs'
@@ -27,6 +49,11 @@ export default [{
 },
 {
   input: 'src/js/books/index.js',
+  plugins: [
+    eslint({
+      /* your options */
+    })
+  ],
   output: {
     file: 'dist/js/chapter.js',
     format: 'cjs'
@@ -34,6 +61,11 @@ export default [{
 },
 {
   input: 'src/js/chat/index.js',
+  plugins: [
+    eslint({
+      /* your options */
+    })
+  ],
   output: {
     file: 'dist/js/chat.js',
     format: 'cjs'
@@ -41,6 +73,11 @@ export default [{
 },
 {
   input: 'src/js/search/index.js',
+  plugins: [
+    eslint({
+      /* your options */
+    })
+  ],
   output: {
     file: 'dist/js/search.js',
     format: 'cjs'
