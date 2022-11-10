8f741bcf1363f1720d10a9816beb6c95522fdd93
sathishk <sathishkumar.thiyagarajan@gmail.com>
2022-11-11 00:04:16 +0530

    Mark My Words

--- src/js/books/Chapter.js
+++ src/js/books/Chapter.js
@@ -15,6 +15,33 @@ class Chapter {
     const bookName = tokens[0];
 
     console.log(bookName);
+
+    const highLightText = () => {
+      var selection = window.getSelection();
+      var parent = selection.anchorNode.parentElement;
+      var selection_text = selection.toString();
+
+      if (selection_text.trim().length !== 0) {
+        if (parent.nodeName === "MARK") {
+          let grandParent = parent.parentElement;
+          parent.replaceWith(document.createTextNode(parent.textContent));
+          grandParent.normalize();
+          return;
+        }
+
+        var mark = document.createElement("mark");
+
+        mark.textContent = selection_text;
+
+        var range = selection.getRangeAt(0);
+        range.deleteContents();
+        range.insertNode(mark);
+      }
+    };
+
+    document.onmouseup = function () {
+      highLightText();
+    };
   }
 }
 
