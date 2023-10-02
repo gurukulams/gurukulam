4d6b2f3d523ca5491380c8587e455b7e9a584fc4
haripriya <rm.haripriya@gmail.com>
2023-10-02 19:39:13 +0530

    Get Ya

--- src/js/practice/screens/QuestionController.js
+++ src/js/practice/screens/QuestionController.js
@@ -5,53 +5,30 @@ export default class QuestionController extends QuestionScreen {
     super();
     this.parent = _parent;
 
-    // Model Objects
-
-    const urlTokens = window.location.pathname.split("/questions/");
-    this.chaptorPath = urlTokens[1];
-
-    this.setupLanguage(urlTokens);
-
-    this.questions = [];
-    this.selectedQuestion = null;
-  }
-
-  setupLanguage(urlTokens) {
-    const elements = document.querySelector(
-      '[aria-labelledby="languageBtn"]'
-    ).children;
-
-    for (let element of elements) {
-      if (element.children[0].dataset.code === "en") {
-        element.children[0].href = "/questions/" + this.chaptorPath;
-      } else {
-        element.children[0].href =
-          "/" +
-          element.children[0].dataset.code +
-          "/questions/" +
-          this.chaptorPath;
-      }
-    }
-
-    if (urlTokens[0] !== "") {
-      const languageCode = urlTokens[0].substring(1);
-
-      for (let element of elements) {
-        if (languageCode === element.children[0].dataset.code) {
-          console.log(element.children[0]);
-
-          document.getElementById("languageBtn").textContent =
-            element.children[0].textContent;
-
-          element.children[0].textContent = "English";
-          element.children[0].setAttribute(
-            "href",
-            "/questions/" + this.chaptorPath
-          );
-
-          break;
+    fetch(this.questionsUrl, {
+      headers: window.ApplicationHeader(),
+    })
+      .then((response) => {
+        // Shorthand to check for an HTTP 2xx response status.
+        // See https://fetch.spec.whatwg.org/#dom-response-ok
+        if (response.ok) {
+          if (response.status === 204) {
+            this.questions = [];
+            this.renderQuestions(this);
+            throw Error("Empty Content 3");
+          }
+          return response.json();
+        } else {
+          // Raise an exception to reject the promise and trigger the outer .catch() handler.
+          // By default, an error response status (4xx, 5xx) does NOT cause the promise to reject!
+          throw Error(response.statusText);
         }
-      }
-    }
+      })
+      .then((data) => {
+        this.setQuestions(window.shuffle(data));
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
   }
 }
--- src/js/practice/screens/QuestionScreen.js
+++ src/js/practice/screens/QuestionScreen.js
@@ -3,6 +3,15 @@ export default class QuestionScreen {
     this.chaptorName = window.location.pathname.split("/questions/")[1];
 
     this.isEditable = false;
+    this.questions = [];
+    this.selectedQuestionIndex = 0;
+
+    // Model Objects
+    const urlTokens = window.location.pathname.split("/questions/");
+    this.chaptorPath = urlTokens[1];
+    this.questionsUrl = "/api/questions/" + this.chaptorPath;
+
+    this.setupLanguage(urlTokens);
 
     // eslint-disable-next-line no-undef
     this.questionEditor = new EasyMDE({
@@ -13,6 +22,8 @@ export default class QuestionScreen {
     this.explanationEditor = new EasyMDE({
       element: document.querySelector("#eTxt"),
     });
+
+    this.answerContainer = document.getElementById("answerContainer");
 
     document
       .querySelectorAll(".editor-toolbar")
@@ -27,22 +38,61 @@ export default class QuestionScreen {
     this.registerEvents();
   }
 
+  setupLanguage(urlTokens) {
+    const elements = document.querySelector(
+      '[aria-labelledby="languageBtn"]'
+    ).children;
+
+    for (let element of elements) {
+      if (element.children[0].dataset.code === "en") {
+        element.children[0].href = "/questions/" + this.chaptorPath;
+      } else {
+        element.children[0].href =
+          "/" +
+          element.children[0].dataset.code +
+          "/questions/" +
+          this.chaptorPath;
+      }
+    }
+
+    if (urlTokens[0] !== "") {
+      const languageCode = urlTokens[0].substring(1);
+      window.LANGUAGE = languageCode;
+
+      for (let element of elements) {
+        if (languageCode === element.children[0].dataset.code) {
+          console.log(element.children[0]);
+
+          document.getElementById("languageBtn").textContent =
+            element.children[0].textContent;
+
+          element.children[0].textContent = "English";
+          element.children[0].setAttribute(
+            "href",
+            "/questions/" + this.chaptorPath
+          );
+
+          break;
+        }
+      }
+    }
+  }
+
   registerEvents() {
     const explainToggleBtn = document.getElementById("explainToggle");
     const explanationContainer = document.getElementById(
       "explanationContainer"
     );
-    const answerContainer = document.getElementById("answerContainer");
 
     explainToggleBtn.addEventListener("click", () => {
-      if (answerContainer.classList.contains("d-none")) {
+      if (this.answerContainer.classList.contains("d-none")) {
         explanationContainer.classList.add("d-none");
-        answerContainer.classList.remove("d-none");
+        this.answerContainer.classList.remove("d-none");
         explainToggleBtn.classList.add("btn-outline-primary");
         explainToggleBtn.classList.remove("btn-primary");
       } else {
         explanationContainer.classList.remove("d-none");
-        answerContainer.classList.add("d-none");
+        this.answerContainer.classList.add("d-none");
         explainToggleBtn.classList.remove("btn-outline-primary");
         explainToggleBtn.classList.add("btn-primary");
       }
@@ -53,10 +103,28 @@ export default class QuestionScreen {
       .parentElement.addEventListener("click", (event) => {
         this.toggleEditor(event);
       });
+
+    document
+      .querySelector("i.fa-check")
+      .parentElement.addEventListener("click", () => {
+        this.answer();
+      });
+
+    this.previousBtn = document.querySelector('[aria-label="Previous"]');
+    this.nextBtn = document.querySelector('[aria-label="Next"]');
+    this.previousBtn.addEventListener("click", () => {
+      this.previous();
+    });
+
+    this.nextBtn.addEventListener("click", () => {
+      this.next();
+    });
   }
 
   toggleEditor(event) {
     this.isEditable = !this.isEditable;
+
+    this.setCurrentQuestion();
 
     this.questionEditor.togglePreview();
     this.explanationEditor.togglePreview();
@@ -101,5 +169,159 @@ export default class QuestionScreen {
         .querySelectorAll(".editor-statusbar")
         .forEach((element) => element.classList.add("d-none"));
     }
+  }
+
+  setQuestions(questions) {
+    this.questions = questions;
+    this.selectedQuestionIndex = 0;
+    this.setCurrentQuestion();
+  }
+
+  previous() {
+    this.selectedQuestionIndex = this.selectedQuestionIndex - 1;
+    this.setCurrentQuestion();
+  }
+
+  next() {
+    this.selectedQuestionIndex = this.selectedQuestionIndex + 1;
+    this.setCurrentQuestion();
+  }
+
+  setCurrentQuestion() {
+    this.setQuestion(this.questions[this.selectedQuestionIndex]);
+
+    if (this.selectedQuestionIndex === this.questions.length - 1) {
+      this.nextBtn.parentElement.classList.add("disabled");
+    } else {
+      this.nextBtn.parentElement.classList.remove("disabled");
+    }
+
+    if (this.selectedQuestionIndex === 0) {
+      this.previousBtn.parentElement.classList.add("disabled");
+    } else {
+      this.previousBtn.parentElement.classList.remove("disabled");
+    }
+  }
+
+  answer() {
+    const selectedQuestion = this.questions[this.selectedQuestionIndex];
+    let selectedCheckBoxes, answers;
+    switch (selectedQuestion.type) {
+      case "CHOOSE_THE_BEST":
+      case "MULTI_CHOICE":
+        selectedCheckBoxes = document.querySelectorAll("input");
+        answers = [];
+        selectedCheckBoxes.forEach((input) => {
+          input.parentElement.parentElement.classList.remove("bg-success");
+          input.parentElement.parentElement.classList.remove("bg-danger");
+          if (input.checked) {
+            answers.push(input.value);
+          }
+        });
+
+        fetch("/api/questions/" + selectedQuestion.id + "/answer", {
+          method: "POST",
+          headers: window.ApplicationHeader(),
+          body: answers.join(","),
+        })
+          .then((response) => {
+            // Shorthand to check for an HTTP 2xx response status.
+            // See https://fetch.spec.whatwg.org/#dom-response-ok
+            if (response.ok) {
+              selectedCheckBoxes.forEach((input) => {
+                if (input.checked) {
+                  input.parentElement.parentElement.classList.add("bg-success");
+                }
+              });
+            } else if (response.status === 406) {
+              selectedCheckBoxes.forEach((input) => {
+                if (input.checked) {
+                  input.parentElement.parentElement.classList.add("bg-danger");
+                }
+              });
+            }
+          })
+          .catch(function (error) {
+            console.log(error);
+          });
+
+        break;
+    }
+  }
+
+  setQuestion(selectedQuestion) {
+    this.questionEditor.value(
+      selectedQuestion.question ? selectedQuestion.question : ""
+    );
+    this.explanationEditor.value(
+      selectedQuestion.explanation ? selectedQuestion.explanation : ""
+    );
+
+    switch (selectedQuestion.type) {
+      case "CHOOSE_THE_BEST":
+        this.setChoices(true, selectedQuestion);
+        break;
+      case "MULTI_CHOICE":
+        this.setChoices(false, selectedQuestion);
+        break;
+    }
+  }
+
+  setChoices(isSingle, selectedQuestion) {
+    this.answerContainer.innerHTML = `<ul class="list-group">
+
+    ${
+      this.isEditable
+        ? `<li class="list-group-item">
+        <input class="form-control me-2" type="search" placeholder="Add New Choice. Press Enter" aria-label="Add New Choice">
+        </li>`
+        : ``
+    }
+
+    
+    
+  </ul>`;
+
+    if (selectedQuestion.choices) {
+      window.shuffle(selectedQuestion.choices);
+    } else {
+      selectedQuestion.choices = [];
+    }
+
+    selectedQuestion.choices.forEach((choice) => {
+      this.setChoice(isSingle, choice);
+    });
+  }
+
+  setChoice(isSingle, choice) {
+    const ulEl = this.answerContainer.firstElementChild;
+    const liEl = document.createElement("li");
+    liEl.classList.add("list-group-item");
+    liEl.classList.add("d-flex");
+    liEl.classList.add("justify-content-between");
+    liEl.classList.add("align-items-center");
+    liEl.innerHTML = `<div class="form-check">
+  <input class="form-check-input" type="${isSingle ? "radio" : "checkbox"}" ${
+      this.isEditable && choice.answer ? "checked" : ""
+    }
+    name="flexRadioDefault" value="${choice.id}" id="flexCheckDefault">
+  <label class="form-check-label" for="flexCheckDefault">
+
+  </label>
+</div>
+${
+  this.isEditable
+    ? `<span class="badge text-dark rounded-pill justify-content-end"><i class="fa-solid fa-pen px-2"></i><i
+    class="far fa-trash-alt" data-bs-toggle="modal" data-bs-target="#exampleModal"></i></span>`
+    : ``
+}
+
+
+
+
+`;
+    ulEl.appendChild(liEl);
+    liEl.firstElementChild.children[1].innerHTML = choice.value;
+    return liEl;
   }
 }
