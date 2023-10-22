e67c7a800ae84350a883f3926d6cc0ee9ec18838
haripriya <rm.haripriya@gmail.com>
2023-10-22 15:20:42 +0530

    SVG  Zoom

--- src/js/books/ImageNotes.js
+++ src/js/books/ImageNotes.js
@@ -8,16 +8,37 @@ class ImageNotes {
       {}
     );
 
+    this.imgEl = document.getElementById("imageModel").querySelector("img");
+    this.figureEl = document
+      .getElementById("imageModel")
+      .querySelector("figure");
+
     document.querySelectorAll("figure>img").forEach((image) => {
       image.addEventListener("dblclick", (event) => {
         this.showImage(event);
       });
     });
+
+    document.querySelectorAll(".goat>svg").forEach((svg) => {
+      svg.addEventListener("dblclick", (event) => {
+        this.showSVG(event);
+      });
+    });
+  }
+
+  showSVG(event) {
+    this.imgEl.classList.add("d-none");
+    this.figureEl.classList.remove("d-none");
+
+    this.figureEl.innerHTML = event.currentTarget.parentElement.innerHTML;
+
+    this.imageModel.show();
   }
 
   showImage(event) {
-    document.getElementById("imageModel").querySelector("img").src =
-      event.currentTarget.src;
+    this.imgEl.src = event.currentTarget.src;
+    this.imgEl.classList.remove("d-none");
+    this.figureEl.classList.add("d-none");
 
     document.getElementById("imageModel").querySelector("h5").innerHTML =
       event.currentTarget.parentElement.querySelector("figcaption").innerHTML;
