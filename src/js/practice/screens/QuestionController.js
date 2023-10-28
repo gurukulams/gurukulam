9cc818e8597ded2348b536201df9c6c6e7da88c5
haripriya <rm.haripriya@gmail.com>
2023-10-28 19:44:22 +0530

    Question Edit Local

--- src/js/practice/screens/QuestionController.js
+++ src/js/practice/screens/QuestionController.js
@@ -4,6 +4,12 @@ export default class QuestionController extends QuestionScreen {
   constructor(_parent) {
     super(_parent);
     if (sessionStorage.auth) {
+      if (JSON.parse(sessionStorage.auth).userName === "tom@email.com") {
+        _parent
+          .querySelector(".fa-pencil")
+          .parentElement.classList.remove("d-none");
+      }
+
       this.loadQuestions();
     } else {
       location.href = "/";
