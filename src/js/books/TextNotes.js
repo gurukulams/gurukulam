a4f5ef2239232aa6db1c0466901bb94dd88c0bff
haripriya <rm.haripriya@gmail.com>
2023-10-30 12:17:13 +0530

    Cleanup UX

--- src/js/books/TextNotes.js
+++ src/js/books/TextNotes.js
@@ -9,9 +9,19 @@ class TextNotes extends CoreNotes {
       document.getElementById("btn-check-outlined")
     );
 
+    const modeIcon = document.querySelector(".fa-pencil");
+
     document
       .querySelector(".fa-pencil")
       .parentElement.classList.remove("d-none");
+
+    document
+      .querySelector(".fa-bezier-curve")
+      .addEventListener("click", (event) => {
+        const temp = event.currentTarget.className;
+        event.currentTarget.className = modeIcon.className;
+        modeIcon.className = temp;
+      });
 
     this.path = window.location.pathname.trim();
 
