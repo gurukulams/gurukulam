52565e6c2df358f08d11159dedd99d5e86328c94
haripriya <rm.haripriya@gmail.com>
2023-11-05 13:03:48 +0530

    DDDDDDD

--- src/js/practice/screens/QuestionController.js
+++ src/js/practice/screens/QuestionController.js
@@ -4,10 +4,8 @@ export default class QuestionController extends QuestionScreen {
   constructor(_parent) {
     super(_parent);
     if (sessionStorage.auth) {
-      if (JSON.parse(sessionStorage.auth).userName === "tom@email.com") {
-        _parent
-          .querySelector(".fa-pencil")
-          .parentElement.classList.remove("d-none");
+      if (JSON.parse(sessionStorage.auth).userName !== "tom") {
+        document.querySelector("i.fa-pencil").classList.add("invisible");
       }
 
       this.loadQuestions();
