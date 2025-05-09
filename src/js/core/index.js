a90ed0bc35a85aff70f9956a651fd9d7bb5e242d
sathishk <sathishkumar.thiyagarajan@gmail.com>
2025-05-09 06:13:09 +0530

    New Layout

--- src/js/core/index.js
+++ src/js/core/index.js
@@ -1,7 +1,5 @@
-class Core {
+class GurukulamsPage {
   constructor() {
-    this.handleModelDialogs();
-
     this.locale = document.getElementById("languageBtn").dataset.code;
 
     if (this.locale === "en") {
@@ -92,79 +90,112 @@ class Core {
       return array;
     };
 
-    this.handleSearch();
     this.handleSecurity();
+    this.handleSearch();
+    this.handleValidation();
+    this.setThemeSetting();
+    this.setScrollIndicator();
+  }
 
-    const popoverTriggerList = document.querySelectorAll(
-      '[data-bs-toggle="popover"]',
-    );
+  handleValidation() {}
 
-    [...popoverTriggerList].map((popoverTriggerEl) => {
-      // eslint-disable-next-line no-undef
-      new bootstrap.Popover(popoverTriggerEl);
+  setThemeSetting() {
+    document.addEventListener("DOMContentLoaded", function () {
+      const themeDropdownButton = document.getElementById("themeDropdown");
+      const themeDropdownItems = document.querySelectorAll(
+        ".dropdown-item[data-theme]",
+      );
+      const icons = {
+        light: "sun-fill",
+        dark: "moon-stars-fill",
+        auto: "circle-half",
+      };
+
+      function setTheme(theme) {
+        let themename;
+        if (theme === "auto") {
+          const prefersDarkScheme = window.matchMedia(
+            "(prefers-color-scheme: dark)",
+          ).matches;
+          theme, (themename = prefersDarkScheme ? "dark" : "light");
+        }
+        console.log(themename);
+
+        document.documentElement.setAttribute("data-bs-theme", theme);
+        themeDropdownButton.innerHTML = `<svg class="bi my-1 theme-icon-active" width="1em" height="1em"><use href="#${icons[theme]}"></use></svg>`;
+      }
+
+      setTheme("auto");
+
+      themeDropdownItems.forEach((item) => {
+        item.addEventListener("click", function (event) {
+          event.preventDefault();
+          const selectedTheme = this.getAttribute("data-theme");
+          setTheme(selectedTheme);
+        });
+      });
     });
   }
 
-  handleModelDialogs() {
-    var myModalEl = document.getElementById("exampleModal");
-    if (myModalEl) {
-      let cRelatedTarget = null;
-      myModalEl.addEventListener("shown.bs.modal", function (event) {
-        cRelatedTarget = event.relatedTarget;
-        myModalEl
-          .querySelector(".btn-primary")
-          .addEventListener("click", (event) => {
-            if (!event.calledFlag) {
-              event.calledFlag = true;
-              const confirmationEvent = new Event("on-confirmation");
-              cRelatedTarget.dispatchEvent(confirmationEvent, {
-                bubbles: false,
-                detail: { text: () => "textarea.value" },
-              });
-              // eslint-disable-next-line no-undef
-              bootstrap.Modal.getInstance(myModalEl).hide();
-            }
-          });
+  setScrollIndicator() {
+    window.addEventListener("scroll", () => {
+      const scrollIndicator = document.getElementById("scrollIndicator");
+      const scrollTop =
+        window.pageYOffset || document.documentElement.scrollTop;
+      const scrollHeight =
+        document.documentElement.scrollHeight -
+        document.documentElement.clientHeight;
+      const scrollPercentage = (scrollTop / scrollHeight) * 100;
+      scrollIndicator.style.width = scrollPercentage + "%";
+    });
+  }
+
+  handleSecurity() {
+    // If User is Logged in
+    if (sessionStorage.auth) {
+      document
+        .querySelector("a.navbar-brand")
+        .setAttribute("data-bs-toggle", "dropdown");
+
+      // data-bs-toggle="dropdown"
+
+      document.getElementById("login-pane").remove("d-none");
+
+      document.getElementById("logoutBtn").addEventListener("click", () => {
+        delete sessionStorage.auth;
+        window.location.reload();
       });
+
+      const userAuth = JSON.parse(sessionStorage.auth);
+
+      document.querySelector(".avatar").src = userAuth.profilePicture;
+
+      document.querySelectorAll(".secured").forEach((el) => {
+        el.classList.remove("secured");
+      });
+
+      const userMenu = document.getElementById("userMenu");
+
+      userMenu.classList.add("dropdown");
+      userMenu.classList.add("dropdown-toggle");
+    } else {
+      // If User is not Logged in
+      if (document.querySelector(".secured") !== null) {
+        document.querySelector(".secured").classList.add("d-none");
+        document.getElementById("login-pane").classList.remove("d-none");
+
+        if (document.querySelector(".fa-google")) {
+          document
+            .querySelector(".fa-google")
+            .parentElement.addEventListener("click", () => {
+              sessionStorage.setItem("ref_page", window.location.href);
+              window.location.href = `/oauth2/authorize/google?redirect_uri=${
+                window.location.protocol + "//" + window.location.host
+              }/welcome`;
+            });
+        }
+      }
     }
-
-    const showStatus = (type, statusMessage) => {
-      var delay = 2000;
-
-      var toastConainerElement = document.getElementById("toast-container");
-      toastConainerElement.innerHTML = `<div class="toast align-items-center  border-0" role="alert" aria-live="assertive" aria-atomic="true">
-      <div class="d-flex">
-        <div class="toast-body text-${type}">
-          ${statusMessage}
-        </div>
-        <button type="button" class="btn-close me-2 m-auto" data-bs-dismiss="toast" aria-label="Close"></button>
-      </div>
-      </div>`;
-      // eslint-disable-next-line no-undef
-      var toast = new bootstrap.Toast(toastConainerElement.firstElementChild, {
-        delay: delay,
-        animation: true,
-      });
-      toast.show();
-
-      // setTimeout(() => toastElement.remove(), delay + 3000); // let a certain margin to allow the "hiding toast animation"
-    };
-
-    window.success = (statusMessage) => {
-      showStatus("success", statusMessage);
-    };
-
-    window.error = (statusMessage) => {
-      showStatus("danger", statusMessage);
-    };
-
-    window.warning = (statusMesaage) => {
-      showStatus("warning", statusMesaage);
-    };
-
-    window.info = (statusMesaage) => {
-      showStatus("info", statusMesaage);
-    };
   }
 
   handleSearch() {
@@ -221,66 +252,8 @@ class Core {
       });
     }
   }
-
-  handleSecurity() {
-    if (sessionStorage.auth) {
-      //this.loadBoards();
-      document.getElementById("login-pane").remove("d-none");
-
-      document.getElementById("logoutBtn").addEventListener("click", () => {
-        delete sessionStorage.auth;
-        window.location.reload();
-      });
-
-      const userAuth = JSON.parse(sessionStorage.auth);
-
-      document.querySelector(".avatar").src = userAuth.profilePicture;
-
-      document.querySelectorAll(".secured").forEach((el) => {
-        el.classList.remove("invisible");
-        el.classList.remove("d-none");
-      });
-    } else {
-      const securedUrls = ["/events"];
-
-      if (securedUrls.includes(window.location.pathname)) {
-        document.body.querySelector("main").innerHTML = `
-        <div class="d-flex align-items-center justify-content-center vh-100">
-            <div class="text-center row">
-                <div class=" col-md-6">
-                    <img src="https://cdn.pixabay.com/photo/2017/03/09/12/31/error-2129569__340.jpg" alt=""
-                        class="img-fluid">
-                </div>
-                <div class=" col-md-6 mt-5">
-                    <p class="fs-3"> <span class="text-danger">Opps!</span> Page not found.</p>
-                    <p class="lead">
-                        The page you’re looking for doesn’t exist.
-                    </p>
-                    <a href="/" class="btn btn-primary">Go Home</a>
-                </div>
-
-            </div>
-        </div>
-        `;
-      }
-
-      if (document.querySelector(".secured") !== null) {
-        document.querySelector(".secured").classList.add("d-none");
-        document.getElementById("login-pane").classList.remove("d-none");
-
-        if (document.querySelector(".fa-google")) {
-          document
-            .querySelector(".fa-google")
-            .parentElement.addEventListener("click", () => {
-              sessionStorage.setItem("ref_page", window.location.href);
-              window.location.href = `/oauth2/authorize/google?redirect_uri=${
-                window.location.protocol + "//" + window.location.host
-              }/welcome`;
-            });
-        }
-      }
-    }
-  }
 }
 
-new Core();
+class HomePage extends GurukulamsPage {}
+
+new HomePage();
