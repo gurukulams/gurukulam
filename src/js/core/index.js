a52a9450d2d73f3d124dd6c194d81e8093bbf8c8
sathishk <sathishkumar.thiyagarajan@gmail.com>
2025-05-12 13:54:42 +0530

    handle Status

--- src/js/core/index.js
+++ src/js/core/index.js
@@ -93,8 +93,49 @@ class GurukulamsPage {
     this.handleSecurity();
     this.handleSearch();
     this.handleValidation();
+    this.handleStatus();
     this.setThemeSetting();
     this.setScrollIndicator();
+  }
+
+  handleStatus() {
+    const showStatus = (type, statusMessage) => {
+      var delay = 2000;
+
+      var toastConainerElement = document.getElementById("toast-container");
+      toastConainerElement.innerHTML = `<div class="toast align-items-center  border-0" role="alert" aria-live="assertive" aria-atomic="true">
+      <div class="d-flex">
+        <div class="toast-body text-${type}">
+          ${statusMessage}
+        </div>
+        <button type="button" class="btn-close me-2 m-auto" data-bs-dismiss="toast" aria-label="Close"></button>
+      </div>
+      </div>`;
+      // eslint-disable-next-line no-undef
+      var toast = new bootstrap.Toast(toastConainerElement.firstElementChild, {
+        delay: delay,
+        animation: true,
+      });
+      toast.show();
+
+      // setTimeout(() => toastElement.remove(), delay + 3000); // let a certain margin to allow the "hiding toast animation"
+    };
+
+    window.success = (statusMessage) => {
+      showStatus("success", statusMessage);
+    };
+
+    window.error = (statusMessage) => {
+      showStatus("danger", statusMessage);
+    };
+
+    window.warning = (statusMesaage) => {
+      showStatus("warning", statusMesaage);
+    };
+
+    window.info = (statusMesaage) => {
+      showStatus("info", statusMesaage);
+    };
   }
 
   handleValidation() {}
