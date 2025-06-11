99b127d8b570f5c56632b18fd773b0200f2dd1cf
VijayWhat <vijayrv042@gmail.com>
2025-06-11 22:58:09 +0530

    Chapter js changes

--- src/js/chapter.js
+++ src/js/chapter.js
@@ -1,47 +1,55 @@
 import TextNotes from "./books/TextNotes";
 import ImageNotes from "./books/ImageNotes";
 import Classes from "./events/Classes";
+class Book {
+  constructor() {
+    console.log("Book class initialized");
+    if(sessionStorage.auth) {
+      console.log("User is authenticated");
+      const contentEl = document.getElementById("content");
 
-if (sessionStorage.auth) {
-  const contentEl = document.getElementById("content");
+      document.getElementById("bookOptionsPane").classList.remove("d-none");
+      document.getElementById("notesBtn").classList.remove("d-none");
 
-  document.getElementById("bookOptionsPane").classList.remove("d-none");
-  document.getElementById("notesBtn").classList.remove("d-none");
+      new TextNotes(contentEl);
 
-  new TextNotes(contentEl);
+      new ImageNotes(contentEl);
+    
+      const classes = new Classes(
+        document.getElementById("event-container"),
+        document.querySelector("i.fa-chalkboard-user").parentElement.dataset.path,
+      );
 
-  new ImageNotes(contentEl);
+      const myOffcanvas = document.getElementById("offcanvas-classes");
+      myOffcanvas.addEventListener("hidden.bs.offcanvas", () => {
+        classes.showEvents();
+      });
 
-  const classes = new Classes(
-    document.getElementById("event-container"),
-    document.querySelector("i.fa-chalkboard-user").parentElement.dataset.path,
-  );
-
-  const myOffcanvas = document.getElementById("offcanvas-classes");
-  myOffcanvas.addEventListener("hidden.bs.offcanvas", () => {
-    classes.showEvents();
-  });
-
-  myOffcanvas.querySelector("i.fa-plus").addEventListener("click", () => {
-    classes.openEvent({});
-  });
-}
-
-handlePageNavigation();
-
-function handlePageNavigation() {
-  const prevLink = document.querySelector(".col-4.text-start a");
-  const nextLink = document.querySelector(".col-4.text-end a");
-
-  document.addEventListener("keydown", function (event) {
-    if (event.key === "ArrowLeft") {
-      if (prevLink) {
-        window.location.href = prevLink.href;
+      myOffcanvas.querySelector("i.fa-plus").addEventListener("click", () => {
+        classes.openEvent({});
+      });
       }
-    } else if (event.key === "ArrowRight") {
-      if (nextLink) {
-        window.location.href = nextLink.href;
+      
+      handlePageNavigation();
+
+      function handlePageNavigation() {
+        
+        const prePage = document.querySelector(".col-4.text-start a");
+        const nextPage = document.querySelector(".col-4.text-end a");
+
+        document.addEventListener("keydown", function (event) {
+          if (event.key === "ArrowLeft") {
+            if (prePage) {
+              window.location.href = prePage.href;
+            }
+          } else if (event.key === "ArrowRight") {
+            if (nextPage) {
+              window.location.href = nextPage.href;
+            }
+          }
+        });
       }
     }
-  });
-}
+  }
+
+new Book();
