952bbd7543df5e33068f0a4136ffc52ed6603e3e
sathishk <sathishkumar.thiyagarajan@gmail.com>
2025-06-21 16:34:52 +0530

    DDD

--- src/js/practice/PracticeScreen.js
+++ src/js/practice/PracticeScreen.js
@@ -3,20 +3,26 @@ import QuestionPane from "./components/QuestionPane";
 export default class PracticeScreen {
   constructor() {
     if (sessionStorage.auth) {
+      this.addActions();
     } else {
       location.href = "/";
     }
+  }
 
+  addActions() {
     const navPane = document.getElementById("navPane");
 
     navPane.querySelectorAll("i").forEach(element => {
       console.log(element);
     });
 
+    navPane.querySelectorAll("a.page-link").forEach(element => {
+      console.log(element);
+    });
   }
 
   doNext() {
-   console.log("Next question");
+    console.log("Next question");
   }
 
   doPrevious() {
