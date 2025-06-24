5b280206d2e8a29ccf7f0fb7ea494284881339f0
VijayWhat <vijayrv042@gmail.com>
2025-06-25 00:45:32 +0530

    Listing the options

--- src/js/practice/components/ChoiceList.js
+++ src/js/practice/components/ChoiceList.js
@@ -1,10 +1,24 @@
 export default class ChoiceList {
-    constructor(isSingle, choices) {
-        const template = document.querySelector(isSingle ? "#radioList" : "#checkboxList");
-        this._element = template.content.cloneNode(true);
-    }
+  constructor(isSingle, choices) {
+    const template = document.querySelector(isSingle ? "#radioList" : "#checkboxList");
 
-    get element()  {
-        return this._element;
-    }
+    this._element = document.createElement("ul");
+    this._element.className = "list-group";
+
+    choices.forEach((choice) => {
+      const clone = template.content.cloneNode(true);
+      const li = clone.querySelector("li");
+      const input = li.querySelector("input");
+      const label = li.querySelector("label");
+
+      input.value = choice.cValue;
+      label.textContent = choice.cValue;
+
+      this._element.appendChild(li);
+    });
+  }
+
+  get element() {
+    return this._element;
+  }
 }
