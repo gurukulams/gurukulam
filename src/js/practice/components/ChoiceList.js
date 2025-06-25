3b337c4674e5ba6092bb9f70fcd7fc9829d10013
sathishk <sathishkumar.thiyagarajan@gmail.com>
2025-06-25 15:57:18 +0530

    T Done

--- src/js/practice/components/ChoiceList.js
+++ src/js/practice/components/ChoiceList.js
@@ -4,17 +4,27 @@ export default class ChoiceList {
 
     this._element = template.content.cloneNode(true).firstChild;
 
+    let length = 0;
+
     if(templateName === "matchesList") {
         choices.forEach((choice, index) => {
             const liEl = this._element.children[index];
             liEl.querySelector("label").textContent = choice.cValue;
+            length++;
         });
     } else {
         choices.forEach((choice, index) => {
             const liEl = this._element.children[index];
             liEl.querySelector("input").value = choice.cValue;
             liEl.querySelector("label").textContent = choice.cValue;
+            length++;
         });
+    }
+    
+    // Remove Unused Items
+    const total = this._element.children.length;
+    for (let index = length; index < total; index++) {
+      this._element.removeChild(this._element.children[length]);
     }
   }
 
